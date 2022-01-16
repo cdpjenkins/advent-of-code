507b4066241f2a73950c188b3c22ca2d4639629c
Chris Jenkins <cdpjenkins@gmail.com>
2022-01-16 18:50:40 +0000

    Day 6

--- 2020/kotlin/src/test/kotlin/utils/Utils.kt
+++ 2020/kotlin/src/test/kotlin/utils/Utils.kt
@@ -3,3 +3,11 @@ package utils
 import java.io.File
 
 fun readLines(day: String) = File("src/main/resources/$day.txt").readLines()
+fun <E> List<E>.splitList(
+    accumulator: List<List<E>> = emptyList(),
+    predicate: (E) -> Boolean
+): List<List<E>> =
+    if (this.isEmpty()) accumulator
+    else this.dropWhile { !predicate(it) }
+        .dropWhile(predicate)
+        .splitList(accumulator + listOf(takeWhile { !predicate(it) }), predicate)

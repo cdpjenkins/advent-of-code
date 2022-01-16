b03d64001d90a8f9b6bd4b3d8f71c524ddda6fc3
cdpjenkins <cdpjenkins@gmail.com>
2022-01-16 15:55:01 +0000

    Day 4 part 2

--- 2020/kotlin/src/test/kotlin/day04/Day04Test.kt
+++ 2020/kotlin/src/test/kotlin/day04/Day04Test.kt
@@ -13,6 +13,18 @@ class Day04Test : FunSpec({
         realInput.numValidPassports() shouldBe 237
     }
 
+    test("day 4 part 2 invalid passports are invalid") {
+        testInputInvalidPassports.numValidPassportsCheckingFields() shouldBe 0
+    }
+
+    test("day 4 part 2 valid passports are valid") {
+        testInputValidPassports.numValidPassportsCheckingFields() shouldBe 4
+    }
+
+    test("day 4 part 2 real data") {
+        realInput.numValidPassportsCheckingFields() shouldBe 172
+    }
+
     test("can parse one passport") {
         testInputWithOnePasswpord.toPassports() shouldBe
                 listOf(
@@ -30,12 +42,43 @@ class Day04Test : FunSpec({
     }
 })
 
-private fun List<String>.numValidPassports() =
+private fun List<String>.numValidPassportsCheckingFields() =
     this.toPassports()
-        .filter { it.isValid() }
+        .filter { it.hasRequiredFields() }
+        .filter { it.requiredFieldsHaveValidValues() }
         .count()
 
-private fun Map<String, String>.isValid() = this.keys.containsAll(listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))
+private fun Map<String, String>.requiredFieldsHaveValidValues() =
+    this["byr"].isValidYear(1920..2002) &&
+            this["iyr"].isValidYear(2010..2020) &&
+            this["eyr"].isValidYear(2020..2030) &&
+            this["hgt"].isValidHgt() &&
+            this["hcl"]!!.matches(hclRegex) &&
+            this["ecl"]!!.matches(eclRegex) &&
+            this["pid"]!!.matches(pidRegex)
+
+fun String?.isValidHgt() =
+    if (this!!.matches(heightRegex)) {
+        val (magnitudeString, unit) = heightRegex.find(this)!!.destructured
+        val magnitude = magnitudeString.toInt()
+        when (unit) {
+            "cm" -> (150..193).contains(magnitude)
+            "in" -> (59..76).contains(magnitude)
+            else -> false
+        }
+    } else {
+        false
+    }
+
+private fun String?.isValidYear(yearRange: IntRange) =
+    this!!.matches("[0-9]{4}".toRegex()) && (yearRange).contains(toInt())
+
+private fun List<String>.numValidPassports() =
+    this.toPassports()
+        .filter { it.hasRequiredFields() }
+        .count()
+
+private fun Map<String, String>.hasRequiredFields() = this.keys.containsAll(listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"))
 private fun List<String>.toPassports() = splitList { it.isEmpty() }.map { it.parsePassport() }
 private fun List<String>.parsePassport() =
     this.joinToString(" ")
@@ -57,6 +100,11 @@ private fun <E> List<E>.splitList(
         .splitList(accumulator + listOf(takeWhile { !predicate(it) }), predicate)
 
 val fieldRegex = "([a-z]{3}):([#0-9a-z]+)".toRegex()
+val heightRegex = "([0-9]{2,3})(cm|in)".toRegex()
+val hclRegex = "#[0-9a-f]{6}".toRegex()
+val eclRegex = "amb|blu|brn|gry|grn|hzl|oth".toRegex()
+val pidRegex = "[0-9]{9}".toRegex()
+
 
 val testInputWithOnePasswpord =
     """
@@ -82,3 +130,34 @@ val testInput =
     """.trimIndent().lines()
 
 val realInput = readLines("Day04")
+
+val testInputInvalidPassports = """
+            eyr:1972 cid:100
+            hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+            iyr:2019
+            hcl:#602927 eyr:1967 hgt:170cm
+            ecl:grn pid:012533040 byr:1946
+
+            hcl:dab227 iyr:2012
+            ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+            hgt:59cm ecl:zzz
+            eyr:2038 hcl:74454a iyr:2023
+            pid:3556412378 byr:2007
+        """.trimIndent().lines()
+
+val testInputValidPassports = """
+            pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+            hcl:#623a2f
+
+            eyr:2029 ecl:blu cid:129 byr:1989
+            iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+            hcl:#888785
+            hgt:164cm byr:2001 iyr:2015 cid:88
+            pid:545766238 ecl:hzl
+            eyr:2022
+
+            iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+        """.trimIndent().lines()

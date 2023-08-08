b512eef8173be20ea314ea728148d04bdf6c1c10
Chris Jenkins <cdpjenkins@gmail.com>
2023-08-08 21:42:11 +0100

    Minor fixes + add a message asking the user to hit ENTER

--- 2022/cpp-aoc-2022/src/util.hpp
+++ 2022/cpp-aoc-2022/src/util.hpp
@@ -1,6 +1,8 @@
 #ifndef CPP_AOC_2022_UTIL_HPP
 #define CPP_AOC_2022_UTIL_HPP
 
+#include <vector>
+
 std::vector<std::string> read_input_file(const char* filename);
 
 #endif //CPP_AOC_2022_UTIL_HPP

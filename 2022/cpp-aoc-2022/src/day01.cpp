9f187b9a6d3351b8f8062b799fbe9197bd726a18
Chris Jenkins <cdpjenkins@gmail.com>
2023-11-12 11:20:31 +0000

    Refactor
    Pull out parsing logic into a single function.
    
    Turns out there was a bug in part 1 where we missed the final elf (but
    we didn't hit it using either test data or real data) :-/

--- 2022/cpp-aoc-2022/src/day01.cpp
+++ 2022/cpp-aoc-2022/src/day01.cpp
@@ -5,25 +5,7 @@
 
 #include "day01.hpp"
 
-int day01_part1_elf_with_most_calories(const std::vector<std::string> &input) {
-    std::vector<int> elves{};
-    int calories = 0;
-    for (auto& line : input) {
-        if (line.empty()) {
-            elves.push_back(calories);
-            calories = 0;
-        } else {
-            int c = std::stoi(line);
-            calories += c;
-        }
-    }
-
-    int max_calories = *(std::max_element(elves.begin(), elves.end()));
-
-    return max_calories;
-}
-
-int day01_part2_three_elves_with_most_calories(const std::vector<std::string> &input) {
+std::vector<int> parse_elves(const std::vector<std::string> &input) {
     std::vector<int> elves{};
     int calories = 0;
     for (auto& line : input) {
@@ -39,10 +21,20 @@ int day01_part2_three_elves_with_most_calories(const std::vector<std::string> &i
         elves.push_back(calories);
     }
 
-    std::sort(elves.begin(), elves.end(), std::greater<>());
+    return elves;
+}
 
+int day01_part1_elf_with_most_calories(const std::vector<std::string> &input) {
+    auto elves{parse_elves(input)};
+    int max_calories = *(std::max_element(elves.begin(), elves.end()));
+
+    return max_calories;
+}
+
+int day01_part2_three_elves_with_most_calories(const std::vector<std::string> &input) {
+    auto elves{parse_elves(input)};
+    std::sort(elves.begin(), elves.end(), std::greater<>());
     int total_calories_of_top_three = std::reduce(elves.begin(), elves.begin() + 3);
 
     return total_calories_of_top_three;
 }
-

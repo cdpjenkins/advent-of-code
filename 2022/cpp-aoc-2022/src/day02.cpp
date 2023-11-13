e7abd67edbda84dc8f923718744fa541d1fc1e22
cdpjenkins <cdpjenkins@gmail.com>
2023-11-13 18:26:31 +0000

    Day 02 WIP

---
+++ 2022/cpp-aoc-2022/src/day02.cpp
@@ -0,0 +1,19 @@
+#include "day02.hpp"
+
+#include <iostream>
+#include <ranges>
+#include <string_view>
+
+#include "util.hpp"
+
+int day02_part1_rock_paper_scissors(const std::vector<std::string> &input) {
+    for (auto& line : input) {
+        std::cout << line << std::endl;
+
+        auto thar_split{std::views::split(line, " ")};
+    }
+
+
+    
+    return 10;
+}

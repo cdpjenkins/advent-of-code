88b80f6b8b64a510b4c095f2fcc495d27255c788
Chris Jenkins <cdpjenkins@gmail.com>
2023-11-23 20:08:45 +0000

    Day 5 part 2

--- 2022/cpp-aoc-2022/src/day05.cpp
+++ 2022/cpp-aoc-2022/src/day05.cpp
@@ -19,6 +19,23 @@ struct Command {
         }
     }
 
+    void execute_on_cratemover_9001(std::vector<std::vector<char>>& stacks) const {
+        std::vector<char> temp;
+
+        for (int i = 0; i < num; i++) {
+            char c = stacks[from].back();
+            stacks[from].pop_back();
+            temp.push_back(c);
+        }
+
+        for (int i = 0; i < num; i++) {
+            char c = temp.back();
+            temp.pop_back();
+
+            stacks[to].push_back(c);
+        }
+    }
+
     friend std::istream &operator>>(std::istream &stream, Command &command) {
         std::string _;
 
@@ -91,5 +108,17 @@ std::string day05_part1(std::istream &input) {
 }
 
 std::string day05_part2(std::istream &input) {
-    return "cheese";
+    std::vector<std::vector<char>> stacks{parse_stacks(input)};
+    std::vector<Command> commands{parse_commands(input)};
+
+    for (const auto &command: commands) {
+        command.execute_on_cratemover_9001(stacks);
+    }
+
+    std::string result;
+    for (const auto &stack: stacks) {
+        result += stack.back();
+    }
+
+    return result;
 }
--- 2022/cpp-aoc-2022/test/day05_test.cpp
+++ 2022/cpp-aoc-2022/test/day05_test.cpp
@@ -7,7 +7,9 @@
 
 using namespace std::string_literals;
 
-std::stringstream s(R"(    [D]
+
+static std::string day_05_test_input{
+R"(    [D]
 [N] [C]
 [Z] [M] [P]
  1   2   3
@@ -15,10 +17,11 @@ std::stringstream s(R"(    [D]
 move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
-move 1 from 1 to 2)");
+move 1 from 1 to 2)"};
 
 TEST_CASE("Day 05 part 1 with test data") {
-    CHECK(day05_part1(s) == "CMZ");
+    std::stringstream input{day_05_test_input};
+    CHECK(day05_part1(input) == "CMZ");
 }
 
 TEST_CASE("Day 05 part 1 with real data") {
@@ -26,11 +29,12 @@ TEST_CASE("Day 05 part 1 with real data") {
     CHECK(day05_part1(input) == "HNSNMTLHQ");
 }
 
-//TEST_CASE("Day 05 part 2 with test data") {
-//    CHECK(day05_part2(s) == "poopoo");
-//}
-//
-//TEST_CASE("Day 05 part 2 with real data") {
-//    std::ifstream input{"../../../advent-of-code-input/2022/day05.txt"};
-//    CHECK(day05_part2(input) == "pooypants");
-//}
+TEST_CASE("Day 05 part 2 with test data") {
+    std::stringstream input{day_05_test_input};
+    CHECK(day05_part2(input) == "MCD");
+}
+
+TEST_CASE("Day 05 part 2 with real data") {
+    std::ifstream input{"../../../advent-of-code-input/2022/day05.txt"};
+    CHECK(day05_part2(input) == "RNLFDJMCT");
+}

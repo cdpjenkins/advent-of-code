2e7269876c6f30620e3c7ced3f62eed52e21f2ca
cdpjenkins <cdpjenkins@gmail.com>
2023-12-06 11:24:35 +0000

    WIP - horriblest hackiest solution
    Turns out it works though.
    
    Needs a tidy up.

--- 2023/kotlin-aoc-2023/src/test/kotlin/day06/Day06.kt
+++ 2023/kotlin-aoc-2023/src/test/kotlin/day06/Day06.kt
@@ -5,9 +5,11 @@ import io.kotest.matchers.doubles.plusOrMinus
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
+import kotlin.math.ceil
+import kotlin.math.floor
 import kotlin.math.sqrt
 
-fun race(buttonTime: Int, totalTime: Int): Int {
+fun race(buttonTime: Long, totalTime: Long): Long {
     return buttonTime * (totalTime - buttonTime)
 }
 
@@ -19,21 +21,43 @@ fun findRootsDouble(time: Double, distance: Double): Pair<Double, Double> {
     val root1 = ((time) - sqrt(time * time - 4 * distance)) / 2
     val root2 = ((time) + sqrt(time * time - 4 * distance)) / 2
 
+    println("root1: ${root1}")
+    println("root2: ${root2}")
+
+
+
     return Pair(root1, root2)
 }
 
-fun bruteForceIt(totalTime: Int, distance: Int): Int {
-    val num = (0..totalTime).map { race(it, totalTime) }.filter { it > distance }.count()
+fun bruteForceIt(totalTime: Long, distance: Long): Long {
+    val num = (0..totalTime).map { race(it, totalTime) }.filter { it > distance }.count().toLong()
     return num
 }
 
+private fun numWaysYouCouldBeatRecord(time: Double, distance: Double): Int {
+    val (root1, root2) = findRootsDouble(time, distance)
+
+    val begin = if (ceil(root1) == floor(root1)) {
+        ceil(root1 + 1)
+    } else {
+        ceil(root1)
+    }.toInt()
+
+    val end = ceil(root2).toInt()
+
+    println("begin: ${begin}")
+    println("end:   ${end}")
+
+    val rangeMeDo = end - begin
+    return rangeMeDo
+}
 
 class Day06Test {
 
     @Test
     fun `part 1 with test input`() {
-        val timeLine = testInput[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
-        val distanceLine = testInput[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
+        val timeLine = testInput[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
+        val distanceLine = testInput[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
 
         println(timeLine)
         println(distanceLine)
@@ -42,7 +66,7 @@ class Day06Test {
 
         println(games)
 
-        val results = games.map { (time, distance) -> bruteForceIt(time, distance) }
+        val results = games.map { (time, distance) -> numWaysYouCouldBeatRecord(time.toDouble(), distance.toDouble()) }
         println(results)
 
         val tharResult = results.reduce { acc, i -> i * acc }
@@ -50,20 +74,17 @@ class Day06Test {
         println(tharResult)
 
         tharResult shouldBe 288
-
-//        val results = games.map { (time, distance) -> race(time, distance) }
-//
-//        println(results)
-//
     }
+
+
 
     @Test
     fun `part 1 with real input`() {
         val input = readInputFileToList("day06.txt")
 
 
-        val timeLine = input[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
-        val distanceLine = input[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toInt() }
+        val timeLine = input[0].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
+        val distanceLine = input[1].split(":")[1].trim().split("\\s+".toRegex()).map { it.toLong() }
 
         println(timeLine)
         println(distanceLine)
@@ -82,27 +103,53 @@ class Day06Test {
         tharResult shouldBe 227850
     }
 
+    @Test
+    fun `part 2 with test input`() {
+        val timeLine = testInput[0].split(":")[1].trim().replace("\\s+".toRegex(), "").toLong()
+        val distanceLine = testInput[1].split(":")[1].trim().replace("\\s+".toRegex(), "").toLong()
+
+
+        val time = timeLine
+        val distance = distanceLine
+
+        val result = bruteForceIt(time, distance)
+        println(result)
+
+
+        result shouldBe 71503
+    }
 
     @Test
-    fun `ston`() {
+    fun `part 2 with real input`() {
+        val input = readInputFileToList("day06.txt")
+
+        val timeLine = input[0].split(":")[1].trim().replace("\\s+".toRegex(), "").toLong()
+        val distanceLine = input[1].split(":")[1].trim().replace("\\s+".toRegex(), "").toLong()
+
+
+        val time = timeLine
+        val distance = distanceLine
+
+        val result = numWaysYouCouldBeatRecord(time.toDouble(), distance.toDouble())
+        println(result)
+
+
+        result shouldBe -1
+
+        fail("arghghgh")
+    }
+
+
+    @Test
+    fun `analytic the races`() {
         numWaysYouCouldBeatRecord(7.0, 9.0) shouldBe 4
         numWaysYouCouldBeatRecord(15.0, 40.0) shouldBe 8
         numWaysYouCouldBeatRecord(30.0, 200.0) shouldBe 9
     }
 
-    private fun numWaysYouCouldBeatRecord(time: Double, distance: Double): Int {
-        val (root1, root2) = findRootsDouble(time, distance)
-
-        val start = Math.ceil(root1).toInt()
-        val end = Math.ceil(root2).toInt()
-
-        val rangeMeDo = end - start
-        return rangeMeDo
-    }
-
     @Test
     fun `try brute force race 1`() {
-        val totalTime = 7
+        val totalTime = 7L
 
         race(0, totalTime) shouldBe 0
         race(1, totalTime) shouldBe 6
@@ -114,12 +161,37 @@ class Day06Test {
         race(7, totalTime) shouldBe 0
     }
 
+    @Test
+    fun `try brute force race 2`() {
+        val totalTime = 15L
+        val distance = 40L
+
+        for (buttonTime in (0..totalTime)) {
+            println("${buttonTime}\t${race(buttonTime, totalTime)}")
+        }
+
+        val num = bruteForceIt(totalTime, distance)
+
+        num shouldBe 8
+    }
+
+
+
+    @Test
+    fun `analytic race 2`() {
+        val totalTime = 15L
+        val distance = 40L
+
+        val num = numWaysYouCouldBeatRecord(totalTime.toDouble(), distance.toDouble())
+
+        num shouldBe 8
+    }
 
 
     @Test
     fun `try brute force race 3`() {
-        val totalTime = 30
-        val distance = 200
+        val totalTime = 30L
+        val distance = 200L
 
         for (buttonTime in (0..totalTime)) {
             println("${buttonTime}\t${race(buttonTime, totalTime)}")
@@ -128,7 +200,6 @@ class Day06Test {
         val num = bruteForceIt(totalTime, distance)
 
         num shouldBe 9
-
     }
 
     @Test

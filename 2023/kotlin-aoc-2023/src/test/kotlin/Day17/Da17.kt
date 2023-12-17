fa9cc9413cb53ba1ce86f769ef5441620b8cee8c
Chris Jenkins <cdpjenkins@gmail.com>
2023-12-17 15:44:16 +0000

    Day 17 part 2
    Man this was tough. Took ages to realise that the path must
    _finish_ on a straight run of a minimum of four blocks.

--- 2023/kotlin-aoc-2023/src/test/kotlin/Day17/Da17.kt
+++ 2023/kotlin-aoc-2023/src/test/kotlin/Day17/Da17.kt
@@ -10,35 +10,62 @@ import kotlin.test.Ignore
 
 private fun part1(input: List<String>): Int {
     val grid = Grid.of(input)
-    val shortestPath = grid.shortestPath()
+    val shortestPath = grid.shortestPath(0 until 3)
 
     return shortestPath.drop(1).sumOf { grid.cost[it.pos]!! }
 }
 
 private fun part2(input: List<String>): Int {
-    return 123
+    val grid = Grid.of(input)
+    val shortestPath = grid.shortestPath(4 until 10)
+
+    return shortestPath.drop(1).sumOf { grid.cost[it.pos]!! }
+}
+
+private fun <E> List<E>.findSegments() {
+    var v = this.first()
+
+    var counter = 0
+    var i = 1
+
+    println()
+    println()
+
+    while (i < this.size) {
+        if (this[i] == v) {
+            counter++
+        } else {
+            println(counter)
+
+            if (counter < 4 || counter > 10) {
+                throw IllegalStateException(counter.toString())
+            }
+
+            counter = 1
+            v = this[i]
+        }
+        i++
+    }
 }
 
 data class Node(
     val pos: Pos,
     val direction: Direction,
-    val stepsLeft: Int
+    val stepsTaken: Int = 0
 ) {
-    fun neighbours(): List<Node> {
 
-        if (this == Node(pos=Pos(x=1, y=7), direction=Direction.DOWN, stepsLeft=0)) {
-            println("poopoo")
-        }
-
+    fun neighbours(validStepsRange: IntRange): List<Node> {
         val turns =
             direction
                 .validTurns()
                 .filter { it != direction }
 
-        val weActuallyTurned = turns.map { Node(pos + it, it, 2) }
+        val weActuallyTurned = turns.map { Node(pos + it, it, 1) }
 
-        if (stepsLeft > 0) {
-            return weActuallyTurned + Node(pos + direction, direction, stepsLeft - 1)
+        if (stepsTaken < validStepsRange.start) {
+            return listOf(Node(pos + direction, direction, stepsTaken + 1))
+        } else if (stepsTaken <= validStepsRange.endInclusive) {
+            return weActuallyTurned + Node(pos + direction, direction, stepsTaken + 1)
         } else {
             return weActuallyTurned
         }
@@ -47,41 +74,34 @@ data class Node(
 
 enum class Direction {
     UP {
-        override fun validTurns(): List<Direction> {
-            return listOf(UP, LEFT, RIGHT)
-        }
+        override fun validTurns() = listOf(UP, LEFT, RIGHT)
 
-        override fun addTo(pos: Pos): Pos {
-            return Pos(pos.x, pos.y - 1)
-        }
+        override fun addTo(pos: Pos) = Pos(pos.x, pos.y - 1)
+
+        override fun toChar() = '^'
     }, DOWN {
-        override fun validTurns(): List<Direction> {
-            return listOf(DOWN, LEFT, RIGHT)
-        }
+        override fun validTurns() = listOf(DOWN, LEFT, RIGHT)
 
-        override fun addTo(pos: Pos): Pos {
-            return Pos(pos.x, pos.y + 1)
-        }
+        override fun addTo(pos: Pos) = Pos(pos.x, pos.y + 1)
+
+        override fun toChar() = 'v'
     }, LEFT {
-        override fun validTurns(): List<Direction> {
-            return listOf(LEFT, UP, DOWN)
-        }
+        override fun validTurns() = listOf(LEFT, UP, DOWN)
 
-        override fun addTo(pos: Pos): Pos {
-            return Pos(pos.x - 1, pos.y)
-        }
+        override fun addTo(pos: Pos) = Pos(pos.x - 1, pos.y)
+
+        override fun toChar() = '<'
     }, RIGHT {
-        override fun validTurns(): List<Direction> {
-            return listOf(RIGHT, UP, DOWN)
-        }
+        override fun validTurns() = listOf(RIGHT, UP, DOWN)
 
-        override fun addTo(pos: Pos): Pos {
-            return Pos(pos.x + 1, pos.y)
-        }
+        override fun addTo(pos: Pos) = Pos(pos.x + 1, pos.y)
+
+        override fun toChar() = '>'
     };
 
     abstract fun validTurns(): List<Direction>
     abstract fun addTo(pos: Pos): Pos;
+    abstract fun toChar(): Char
 }
 
 data class Pos(val x: Int, val y: Int) {
@@ -96,20 +116,23 @@ data class Pos(val x: Int, val y: Int) {
 }
 
 data class Grid(val cost: Map<Pos, Int>) {
-    private val width: Int = cost.keys.maxOf { it.x } + 1
-    private val height: Int = cost.keys.maxOf { it.y } + 1
+    val width: Int = cost.keys.maxOf { it.x } + 1
+    val height: Int = cost.keys.maxOf { it.y } + 1
 
     val startPos = Pos(0, 0)
     val target: Pos = Pos(width - 1, height - 1)
 
     private fun heuristic(a: Pos): Int {
         // this turns the algo from A* into Dijkstra's ... oops
-        return 0 //a.manhattenDistanceTo(target)
+        return 0 // a.manhattenDistanceTo(target)
     }
 
-    fun shortestPath(): List<Node> {
+    // TODO - an IntRange actually might not be the best thing to use ere. It's not like there's a range and we just
+    // need to be within that range. Rather, we do different things depending on whether we are before, inside or
+    // after the range.
+    fun shortestPath(validStepsRange: IntRange): List<Node> {
         // A* FTW
-        val startNode = Node(startPos, Direction.RIGHT, 3)
+        val startNode = Node(startPos, Direction.RIGHT)
 
         val previousMap: MutableMap<Node, Node> = mutableMapOf()
         val gScore: MutableMap<Node, Int> = mutableMapOf(startNode to 0)
@@ -122,10 +145,15 @@ data class Grid(val cost: Map<Pos, Int>) {
             val current = openSet.remove()
 
             if (current.pos == target) {
+                if (current.stepsTaken < validStepsRange.start) {
+                    // alas we can't stop!
+                    continue
+                }
+
                 return reconstructPath(current, previousMap)
             }
 
-            val neighbours = current.neighbours()
+            val neighbours = current.neighbours(validStepsRange)
             val neighboursInBounds = neighbours
                 .filter { it.pos in cost.keys }
 
@@ -168,7 +196,8 @@ data class Grid(val cost: Map<Pos, Int>) {
             (0 until width).map { x ->
                 val pos = Pos(x, y)
                 if (pos in pathElemrnts.map { it.pos } ) {
-                    '#'
+                    pathElemrnts.first{ it.pos == pos}.direction.toChar()
+//                    '#'
                 } else {
                     cost[pos]
                 }
@@ -195,42 +224,76 @@ class Day17Test {
         part1(testInput) shouldBe 102
     }
 
+    @Ignore
     @Test
     fun `part 1 with real input`() {
         part1(readInputFileToList("day17.txt")) shouldBe 722
     }
 
-//    @Test
-//    fun `part 2 with test input`() {
-//        part2(testInput) shouldBe -1
-//    }
-//
-//    @Test
-//    fun `part 2 with real input`() {
-//        part2(readInputFileToList("day17.txt")) shouldBe -1
-//    }
+    @Test
+    fun `part 2 with test input`() {
+        part2(testInput) shouldBe 94
+    }
+
+    @Ignore
+    @Test
+    fun `part 2 with real input`() {
+
+        // 889 is too low
+
+
+        // 894 ???
+
+        part2(readInputFileToList("day17.txt")) shouldBe 894
+    }
+
+    @Test
+    fun `part 2 shorted path on pathalogical input`() {
+        val grid = Grid.of(
+            """
+                111111111111
+                999999999991
+                999999999991
+                999999999991
+                999999999991
+            """.trimIndent().lines()
+        )
+
+        val shortestPath = grid.shortestPath(4 until 10)
+
+        grid.asStringWithPath(shortestPath) shouldBe
+                """
+                    >>>>>>>>1111
+                    9999999v9991
+                    9999999v9991
+                    9999999v9991
+                    9999999v>>>>
+                """.trimIndent()
+
+        shortestPath.drop(1).sumOf { grid.cost[it.pos]!! } shouldBe 71
+    }
 
     @Test
     fun `can find correct shortest path through test data`() {
         val grid = Grid.of(testInput)
 
-        val shortestPath = grid.shortestPath()
+        val shortestPath = grid.shortestPath(0 until 3)
 
         grid.asStringWithPath(shortestPath) shouldBe
                 """
-                    ###34####1323
-                    32####35#5623
-                    32552456###54
-                    3446585845#52
-                    4546657867##6
-                    14385987984#4
-                    44578769877#6
-                    36378779796##
-                    465496798688#
-                    456467998645#
-                    12246868655##
-                    25465488877#5
-                    43226746555##
+                    >>>34^>>>1323
+                    32v>>>35v5623
+                    32552456v>>54
+                    3446585845v52
+                    4546657867v>6
+                    14385987984v4
+                    44578769877v6
+                    36378779796v>
+                    465496798688v
+                    456467998645v
+                    12246868655<v
+                    25465488877v5
+                    43226746555v>
                 """.trimIndent()
     }
 
@@ -244,11 +307,11 @@ class Day17Test {
             """.trimIndent().lines()
         )
 
-        grid.asStringWithPath(grid.shortestPath()) shouldBe
+        grid.asStringWithPath(grid.shortestPath(0 until 3)) shouldBe
                 """
-                    ##9
-                    9##
-                    99#
+                    >>9
+                    9v>
+                    99v
                 """.trimIndent()
     }
 
@@ -268,17 +331,17 @@ class Day17Test {
             """.trimIndent().lines()
         )
 
-        grid.asStringWithPath(grid.shortestPath()) shouldBe
+        grid.asStringWithPath(grid.shortestPath(0 until 3)) shouldBe
                 """
-                    #6
-                    #4
-                    #6
-                    ##
-                    8#
-                    5#
-                    ##
-                    #5
-                    ##
+                    >6
+                    v4
+                    v6
+                    v>
+                    8v
+                    5v
+                    <v
+                    v5
+                    v>
                 """.trimIndent()
     }
 }

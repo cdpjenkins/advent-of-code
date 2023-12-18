26c77d7ae17b5ae68b2b52167bc9d695d54bade7
cdpjenkins <cdpjenkins@gmail.com>
2023-12-18 23:25:10 +0000

    Day 18 part 2

--- 2023/kotlin-aoc-2023/src/test/kotlin/day18/Day18.kt
+++ 2023/kotlin-aoc-2023/src/test/kotlin/day18/Day18.kt
@@ -5,14 +5,163 @@ import RegexUtils.parseUsingRegex
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.math.max
+import kotlin.math.min
 
-private fun part1(input: List<String>): Int {
+private fun Pair<Int, Int>.sorted() = if (first < second) this else second to first
 
-    val lagoon = parseLagoon(input)
+private fun part1(input: List<String>): Long {
+    val instructions = input.map { Instruction.parseUsingPart1Rules(it) }
 
-    lagoon.digOutInterior()
+    return instructions.findAreaTheCleverWay()
+}
 
-    return lagoon.count()
+private fun part2(input: List<String>): Long {
+    val instructions = input.map { Instruction.parseUsingPart2Rules(it) }
+
+    return instructions.findAreaTheCleverWay()
+}
+
+private fun List<Instruction>.findAreaTheCleverWay(): Long {
+    var x = 0L
+    var y = 0L
+    var area = 0L
+    var perimeterLength = 0L
+
+    forEach { instruction ->
+        perimeterLength += instruction.distance
+
+        when (instruction.direction) {
+            Direction.R -> {
+                x += instruction.distance
+                area -= y * instruction.distance
+            }
+
+            Direction.D -> {
+                y += instruction.distance
+            }
+
+            Direction.L -> {
+                x -= instruction.distance
+                area += y * instruction.distance
+            }
+
+            Direction.U -> {
+                y -= instruction.distance
+            }
+        }
+    }
+
+    println("area: $area")
+    println("perimeter length: " + perimeterLength)
+
+    return area + (perimeterLength / 2) + 1
+}
+
+private fun part1_meh(input: List<String>): Int {
+    val lines = Line.parseToLines(input)
+
+    val verticalLines = lines.filter { it.direction == Direction.U || it.direction == Direction.D }
+
+    val sortedInterestingYs =
+        (verticalLines.map { it.start.y } +
+            verticalLines.map { it.end.y })
+            .toSet().sorted()
+
+    val yIntervals = sortedInterestingYs.zipWithNext().map { it.sorted() }
+
+    val slicedIntervals = sliceThoseIntervals(yIntervals)
+
+    val area = slicedIntervals.map { (start, end) ->
+        println("$start to $end")
+
+        val linesThatIntersect = verticalLines.filter { it.intersectsWith(y = start) }.sortedBy { it.start.x }.toList()
+
+        println(linesThatIntersect)
+
+        val horizThangs = horizontalRangeThingies(linesThatIntersect)
+
+        val horizontalGroundCovered = horizThangs.sumOf { it.endInclusive + 1 - it.start }
+
+        println(horizontalGroundCovered)
+
+        horizontalGroundCovered * (end + 1 - start)
+    }.sum()
+
+    val maxY = lines.filter { it.direction == Direction.L }.maxBy { it.start.y }
+    println("maxY: $maxY")
+
+    return area
+}
+
+// TODO meh just use pairs
+fun horizontalRangeThingies(inScope: List<Line>): List<IntRange> {
+    val rangeThingies = mutableListOf<IntRange>()
+    var remaining = inScope
+
+    while (remaining.isNotEmpty()) {
+        val startThang = remaining.first()
+        require(startThang.direction == Direction.U) {
+            println("bad thang: $inScope")
+            "urgh ${startThang}"
+        }
+        val gotRidOfAllTheStarts = remaining.
+            dropWhile { it.direction == Direction.U }
+        val endThang = gotRidOfAllTheStarts.takeWhile { it.direction == Direction.D }.last()
+
+        rangeThingies.add(startThang.start.x..endThang.start.x)
+        remaining = gotRidOfAllTheStarts.dropWhile { it.direction == Direction.D }
+    }
+
+    return rangeThingies.toList()
+}
+
+private fun sliceThoseIntervals(yIntervals: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+    val slicedIntervals: List<Pair<Int, Int>> = yIntervals.flatMap {
+        val (start, end) = it
+
+        if (end - start == 0) {
+            listOf(it)
+        } else if (end - start == 1) {
+            listOf(start to start)
+        } else {
+            listOf(start to start, (start + 1) to (end - 1))
+        }
+    } + (yIntervals.last().second to yIntervals.last().second)
+    return slicedIntervals
+}
+
+private fun List<Instruction>.toLines(): Sequence<Line> {
+    var pos = Pos(0, 0)
+    val lines = sequence {
+        forEach {
+            val newPos = pos + it
+            this.yield(Line(pos, newPos, it.direction))
+
+            pos = newPos
+        }
+    }
+    return lines
+}
+
+data class Line(
+    val start: Pos,
+    val end: Pos,
+    val direction: Direction
+) {
+    companion object {
+        fun parseToLines(input: List<String>): Sequence<Line> {
+            val instructions = input.map { Instruction.parseUsingPart1Rules(it) }
+
+            return instructions.toLines()
+        }
+    }
+
+    fun intersectsWith(y: Int): Boolean {
+        check((direction == Direction.U || direction == Direction.D)) { direction.toString() }
+
+        return y in (min(start.y, end.y)..max(start.y, end.y))
+    }
 }
 
 private fun MutableMap<Pos, Char>.digOutInterior() {
@@ -33,28 +182,9 @@ private fun MutableMap<Pos, Char>.digOutInterior() {
             }
         }
     }
-
-//    for (y in (minY()..maxY())) {
-//        var inside = false
-//        for (x in (minX()..maxX())) {
-//            if (y == 1) {
-//                println("poopoo")
-//            }
-//
-//            val pos = Pos(x, y)
-//            if (this[pos] == '#') {
-//                inside = !inside
-//            }
-//
-//            if (inside) {
-//                this[pos] = '#'
-//            }
-//        }
-//    }
 }
 
 private fun MutableMap<Pos, Char>.floodFill(initialPos: Pos) {
-
     val minX = minX()
     val maxX = maxX()
     val minY = minY()
@@ -84,7 +214,6 @@ private fun MutableMap<Pos, Char>.floodFill(initialPos: Pos) {
                         it.y < maxY + 3
             }.forEach { stack.push(it) }
         }
-
     }
 }
 
@@ -99,20 +228,13 @@ private fun MutableMap<Pos, Char>.asString(): String {
 }
 
 private fun parseLagoon(input: List<String>): MutableMap<Pos, Char> {
-    val regex = "^([UDLR]) (\\d+) \\((#[a-f0-9]{6})\\)$".toRegex()
-
-    val instructions = input.map {
-        val (directionString, distanceString, colourString) = it.parseUsingRegex(regex)
-
-        Instruction(Direction.valueOf(directionString), distanceString.toInt(), colourString)
-    }
+    val instructions = input.map { Instruction.parseUsingPart1Rules(it) }
 
     val lagoon = mutableMapOf<Pos, Char>()
     var currentLocation = Pos(0, 0)
     instructions.forEach { currentLocation = it.execute(lagoon, currentLocation) }
     return lagoon
 }
-
 
 fun MutableMap<Pos, Char>.minX() = minOf { (k, v) -> k.x }
 fun MutableMap<Pos, Char>.maxX() = maxOf { (k, v) -> k.x }
@@ -126,13 +248,39 @@ data class Pos(
     operator fun plus(direction: Direction): Pos {
         return Pos(x + direction.dx, y + direction.dy)
     }
+
+    operator fun plus(instruction: Instruction): Pos {
+        return Pos(
+            x + instruction.direction.dx * instruction.distance,
+            y + instruction.direction.dy * instruction.distance
+        )
+    }
 }
 
 data class Instruction(
     val direction: Direction,
     val distance: Int,
-    val colourString: String
 ) {
+    companion object {
+        val part1Regex = "^([UDLR]) (\\d+) \\((#[a-f0-9]{6})\\)$".toRegex()
+
+        fun parseUsingPart1Rules(it: String): Instruction {
+            val (directionString, distanceString, _) = it.parseUsingRegex(part1Regex)
+
+            return Instruction(Direction.valueOf(directionString), distanceString.toInt())
+        }
+
+        val part2Regex = "^[UDLR] \\d+ \\(#([a-f0-9]{5})([a-f0-9])\\)$".toRegex()
+
+        fun parseUsingPart2Rules(it: String): Instruction {
+            val (distanceHexString, directionHexString) = it.parseUsingRegex(part2Regex)
+
+            val direction = Direction.fromHex(directionHexString)
+
+            return Instruction(direction, distanceHexString.toInt(16))
+        }
+    }
+
     fun execute(lagoon: MutableMap<Pos, Char>, currentLocation: Pos): Pos {
         var p = currentLocation
         for (i in (0 until distance)) {
@@ -143,10 +291,6 @@ data class Instruction(
 
         return p
     }
-}
-
-private fun part2(input: List<String>): Int {
-    return 123
 }
 
 enum class Direction {
@@ -167,6 +311,17 @@ enum class Direction {
     abstract val dx: Int
     abstract val dy: Int
 
+    companion object {
+        fun fromHex(hex: String): Direction {
+            return when (hex) {
+                "0" -> R
+                "1" -> D
+                "2" -> L
+                "3" -> U
+                else -> throw IllegalArgumentException(hex)
+            }
+        }
+    }
 }
 
 class Day18Test {
@@ -179,18 +334,16 @@ class Day18Test {
     fun `part 1 with real input`() {
         part1(readInputFileToList("day18.txt")) shouldBe 56923
     }
-//
-//    @Ignore
-//    @Test
-//    fun `part 2 with test input`() {
-//        part2(testInput) shouldBe -1
-//    }
-//
-//    @Ignore
-//    @Test
-//    fun `part 2 with real input`() {
-//        part2(readInputFileToList("day_template.txt")) shouldBe -1
-//    }
+
+    @Test
+    fun `part 2 with test input`() {
+        part2(testInput) shouldBe 952408144115L
+    }
+
+    @Test
+    fun `part 2 with real input`() {
+        part2(readInputFileToList("day18.txt")) shouldBe -1
+    }
 
     @Test
     fun `can parse and draw lagoon outline from test input`() {
@@ -209,7 +362,7 @@ class Day18Test {
         """.trimIndent()
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun `can dig out the interior from test input`() {
         val lagoon = parseLagoon(testInput)
 
@@ -228,6 +381,40 @@ class Day18Test {
                     .######
                     .######
                 """.trimIndent()
+    }
+
+    @Test
+    fun `can slice up intervals`() {
+        val unsliced = listOf(
+            0 to 1,
+            1 to 3,
+            3 to 10
+        )
+
+        val sliced = sliceThoseIntervals(unsliced)
+
+        sliced shouldBe listOf(
+            0 to 0,
+            1 to 1,
+            2 to 2,
+            3 to 3,
+            4 to 9,
+            10 to 10
+        )
+    }
+
+    @Test
+    fun `can dudify ranges`() {
+        val someLines = listOf(
+            Line(Pos(0, 0), Pos(0, 0), Direction.U),
+            Line(Pos(10, 0), Pos(10, 0), Direction.U),
+            Line(Pos(20, 0), Pos(20, 0), Direction.D),
+            Line(Pos(30, 0), Pos(30, 0), Direction.U),
+            Line(Pos(40, 0), Pos(40, 0), Direction.D)
+        )
+
+        horizontalRangeThingies(someLines) shouldBe listOf(0..20, 30..40)
+
     }
 }
 

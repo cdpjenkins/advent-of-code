04783a5d4f7397d98e8d79ca890c0fd2832320b4
cdpjenkins <cdpjenkins@gmail.com>
2023-12-28 09:07:38 +0000

    Day 21 part 2

--- 2023/kotlin-aoc-2023/src/test/kotlin/day21/Day21.kt
+++ 2023/kotlin-aoc-2023/src/test/kotlin/day21/Day21.kt
@@ -14,6 +14,58 @@ private fun part1(input: List<String>, numSteps: Int = 6): Int {
         .size
 }
 
+private fun part2(input: List<String>): Long {
+    // See https://github.com/villuna/aoc23/wiki/A-Geometric-solution-to-advent-of-code-2023,-day-21
+    // for the theory behind this (which I did _not_ work out by myself)
+
+    val graph = input.parseGraph()
+
+    val lala = generateSequence(startSet(graph)) { step(it, graph) }
+        .accumulateSteps(graph)
+        .take(134 + 134)
+        .toList().last()
+
+    val numEvenStepsInFullSquare = lala.filter { (_, n) -> n % 2 == 0 }.count()
+    val numOddStepsInFullSquare = lala.filter { (_, n) -> n % 2 == 1 }.count()
+
+    val stepsInEvenCorner = lala.filter { (_, n) -> n % 2 == 0 && n > 65 }.count()
+    val stepsInOddCorner = lala.filter { (_, n) -> n % 2 == 1 && n > 65 }.count()
+
+    val n = 202300L
+
+    val numEvenSquares = n * n
+    val numOddSquares = (n + 1) * (n + 1)
+    val numOddCorners = n + 1
+    val numEvenCorners = n
+
+    return numOddSquares * numOddStepsInFullSquare +
+            numEvenSquares * numEvenStepsInFullSquare -
+            (numOddCorners * stepsInOddCorner) +
+            (numEvenCorners * stepsInEvenCorner)
+}
+
+// this is a horrifically inefficient way of doing this!
+fun Sequence<Map<Point, Int>>.accumulateSteps(graph: Map<Point, Char>): Sequence<Map<Point, Int>> {
+    var accumulated = mapOf<Point, Int>()
+
+    val ston = this
+
+    return sequence {
+        ston.forEach {
+            val filterNotNull = graph.map { (p, _) ->
+                when (p) {
+                    in accumulated -> p to accumulated[p]!!
+                    in it -> p to it[p]!!
+                    else -> null
+                }
+            }.filterNotNull()
+                .toMap()
+            accumulated = filterNotNull
+            yield(accumulated)
+        }
+    }
+}
+
 fun startSet(graph: Map<Point, Char>): Map<Point, Int> =
     graph
         .entries
@@ -34,7 +86,7 @@ fun step(points: Map<Point, Int>, graph: Map<Point, Char>): Map<Point, Int> =
         .toMap()
 
 data class Point(val x: Int, val y: Int) {
-    fun neighbours(): Set<Point> {
+    private fun neighbours(): Set<Point> {
         return setOf(
             Point(this.x, this.y - 1),
             Point(this.x, this.y + 1),
@@ -50,10 +102,6 @@ data class Point(val x: Int, val y: Int) {
     }
 }
 
-private fun part2(input: List<String>): Int {
-    return 123
-}
-
 class Day21Test {
     @Test
     fun `part 1 with test input`() {
@@ -65,16 +113,10 @@ class Day21Test {
         part1(readInputFileToList("day21.txt"), numSteps = 64) shouldBe 3699
     }
 
-    @Ignore
-    @Test
-    fun `part 2 with test input`() {
-        part2(testInput) shouldBe -1
-    }
-
-    @Ignore
+    @Ignore // too horrifically inefficient to run every time
     @Test
     fun `part 2 with real input`() {
-        part2(readInputFileToList("day_template.txt")) shouldBe -1
+        part2(readInputFileToList("day21.txt")) shouldBe 613391294577878L
     }
 
     @Test
@@ -180,7 +222,29 @@ class Day21Test {
                     .##6.##.##.
                     ...........
                 """.trimIndent()
+    }
 
+    @Test
+    fun `even and odd parity squares`() {
+        val graph = testInput.parseGraph()
+        val start = startSet(graph)
+
+        val stepList = generateSequence(start) { step(it, graph) }
+            .take(30)
+            .toList()
+
+        stepList[9].size shouldBe 29
+        stepList[10].size shouldBe 33
+        stepList[11].size shouldBe 35
+        stepList[12].size shouldBe 40
+        stepList[13].size shouldBe 39
+        stepList[14].size shouldBe 42
+        stepList[15].size shouldBe 39
+        stepList[16].size shouldBe 42
+        stepList[17].size shouldBe 39
+        stepList[18].size shouldBe 42
+        stepList[19].size shouldBe 39
+        stepList[20].size shouldBe 42
     }
 }
 

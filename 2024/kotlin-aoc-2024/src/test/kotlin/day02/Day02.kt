f06e40103381184476a4a2c27abee034803534de
Chris Jenkins <cdpjenkins@gmail.com>
2024-12-02 18:38:02 +0000

    Day 2 tidy up a little

--- 2024/kotlin-aoc-2024/src/test/kotlin/day02/Day02.kt
+++ 2024/kotlin-aoc-2024/src/test/kotlin/day02/Day02.kt
@@ -17,7 +17,7 @@ private fun String.parseReport() = this.split(" ").map { it.toInt() }
 private fun List<Int>.isSafe() = this.isAllAscendingGradually() || this.isAllDescendingGradually()
 private fun List<Int>.isAllAscendingGradually() = this.zipWithNext().all { (a, b) -> a - b in (-3..-1) }
 private fun List<Int>.isAllDescendingGradually() = this.zipWithNext().all { (a, b) -> a - b in (1..3) }
-private fun List<Int>.isSafeWithDampening(): Boolean = indices.any { i -> withElementRemovedAt(i).isSafe() }
+private fun List<Int>.isSafeWithDampening() = indices.any { i -> withElementRemovedAt(i).isSafe() }
 private fun List<Int>.withElementRemovedAt(i: Int) = this.toMutableList().apply { removeAt(i) }.toList()
 
 class Day02Test {

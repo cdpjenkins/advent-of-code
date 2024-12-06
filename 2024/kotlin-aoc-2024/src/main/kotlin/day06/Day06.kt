092a6860ddc63c3a96178966e06245fbc4e6a57c
cdpjenkins <cdpjenkins@gmail.com>
2024-12-06 22:10:14 +0000

    Tidy up
    Move non-test code to non-test sourcefile.

---
+++ 2024/kotlin-aoc-2024/src/main/kotlin/day06/Day06.kt
@@ -0,0 +1,137 @@
+package day06
+
+fun part1(input: List<String>): Int {
+    val (map, start) = input.parse()
+    val poses = pathUntilWeFallOffTheMap(start, map)
+
+    return poses.size
+}
+
+fun part2(input: List<String>): Int {
+    val (map, start) = input.parse()
+    val poses = pathUntilWeFallOffTheMap(start, map) - start.position
+
+    return poses.count { resultsInCycle(start, map + (it to 'O')) }
+}
+
+private fun Map<Point2D, Char>.isObstacleAt(it: Point2D) = this[it] == '#' || this[it] == 'O'
+
+fun pathUntilWeFallOffTheMap(
+    start: Guard,
+    map: Map<Point2D, Char>,
+): Set<Point2D> =
+    generateSequence(start) { it.move(map) }
+        .takeWhile { it.isWithinBoundsOf(map) }
+        .map { it.position }
+        .toSet()
+
+fun pathUntilWeDetectACycle(
+    start: Guard,
+    map: Map<Point2D, Char>
+): Set<Point2D> {
+    val seq = generateSequence(start) { it.move(map) }
+
+    val seenAlready = mutableSetOf<Guard>()
+    val path = mutableListOf<Guard>()
+    run fromLook@ {
+        seq.forEach {
+            if (it in seenAlready) {
+                return@fromLook
+            } else if (!it.isWithinBoundsOf(map)) {
+                return@fromLook
+            } else {
+                seenAlready.add(it)
+                path.add(it)
+            }
+        }
+    }
+
+    return path.map { it.position }.toSet()
+}
+
+fun resultsInCycle(
+    start: Guard,
+    map: Map<Point2D, Char>,
+): Boolean {
+    val seq = generateSequence(start) { it.move(map) }
+
+    val seenAlready = mutableSetOf<Guard>()
+
+    seq.forEach {
+        if (it in seenAlready) {
+            return true
+        } else if (!it.isWithinBoundsOf(map)) {
+            return false
+        } else {
+            seenAlready.add(it)
+        }
+    }
+
+    // we should never get here - but meh
+    return false
+}
+
+fun List<String>.parse(): Pair<Map<Point2D, Char>, Guard> {
+    val map = flatMapIndexed { y, line ->
+        line.mapIndexed { x, c -> Point2D(x, y) to c }
+    }.toMap()
+
+    val start = map.entries.firstOrNull { (p, c) -> c == '^' }!!.key
+
+    return Pair(map, Guard(start, Direction.UP))
+}
+
+fun asString(map: Map<Point2D, Char>, poses: Set<Point2D>): String {
+    return (0 .. map.keys.maxOf { it.y }).map { y ->
+        (0 .. map.keys.maxOf { it.x }).map { x ->
+            if (Point2D(x, y) in poses) {
+                'X'
+            } else {
+                map[Point2D(x, y)]
+            }
+        }.joinToString("")
+    }.joinToString("\n")
+}
+
+data class Guard(val position: Point2D, val direction: Direction) {
+    fun move(map: Map<Point2D, Char>): Guard {
+        val newDirection = maybeTurn(map)
+        return Guard(newDirection.move(position), newDirection)
+    }
+
+    private fun maybeTurn(map: Map<Point2D, Char>): Direction =
+        if (map.isObstacleAt(direction.move(position))) Guard(position, direction.turnRight()).maybeTurn(map)
+        else direction
+
+    fun isWithinBoundsOf(map: Map<Point2D, Char>): Boolean {
+        return position in map.keys
+    }
+}
+
+data class Point2D(val x: Int, val y: Int)
+enum class Direction {
+    UP {
+        override fun turnLeft() = LEFT
+        override fun turnRight() = RIGHT
+        override fun move(p: Point2D) = Point2D(p.x, p.y - 1)
+    },
+    DOWN {
+        override fun turnLeft() = RIGHT
+        override fun turnRight() = LEFT
+        override fun move(p: Point2D) = Point2D(p.x, p.y + 1)
+    },
+    LEFT {
+        override fun turnLeft() = DOWN
+        override fun turnRight() = UP
+        override fun move(p: Point2D) = Point2D(p.x - 1, p.y)
+    },
+    RIGHT {
+        override fun turnLeft() = UP
+        override fun turnRight() = DOWN
+        override fun move(p: Point2D) = Point2D(p.x + 1, p.y)
+    };
+
+    abstract fun turnLeft(): Direction
+    abstract fun turnRight(): Direction
+    abstract fun move(p: Point2D): Point2D
+}
--- 2024/kotlin-aoc-2024/src/test/kotlin/day06/Day06Test.kt
+++ 2024/kotlin-aoc-2024/src/test/kotlin/day06/Day06Test.kt
@@ -3,113 +3,6 @@ package day06
 import FileUtil.readInputFileToList
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import kotlin.test.Ignore
-
-private fun part1(input: List<String>): Int {
-    val (map, start) = input.parse()
-    val poses = path(start, map)
-
-    return poses.size
-}
-
-private fun part2(input: List<String>): Int {
-    val (map, start) = input.parse()
-    val poses = path(start, map)
-
-    val posesThangie = poses - start
-
-
-
-    return 123
-}
-
-private fun path(
-    start: Point2D,
-    map: Map<Point2D, Char>,
-): Set<Point2D> =
-    generateSequence(Guard(start, Direction.UP)) { it.move(map) }
-        .takeWhile { it.isWithinBoundsOf(map) }
-        .map { it.p }
-        .toSet()
-
-private fun pathNoCycles(
-    start: Point2D,
-    map: Map<Point2D, Char>,
-): Set<Point2D> =
-    generateSequence(Guard(start, Direction.UP)) { it.move(map) }
-        .takeWhile { it.isWithinBoundsOf(map) && it.p != start }
-        .map { it.p }
-        .toSet()
-
-private fun List<String>.parse(): Pair<Map<Point2D, Char>, Point2D> {
-    val map = flatMapIndexed { y, line ->
-        line.mapIndexed { x, c -> Point2D(x, y) to c }
-    }.toMap()
-
-    val start = map.entries.firstOrNull { (p, c) -> c == '^' }!!.key
-
-    return Pair(map, start)
-}
-
-fun asString(map: Map<Point2D, Char>, poses: Set<Point2D>): String {
-    return (0 .. map.keys.maxOf { it.y }).map { y ->
-        (0 .. map.keys.maxOf { it.x }).map { x ->
-            if (Point2D(x, y) in poses) {
-                'X'
-            } else {
-                map[Point2D(x, y)]
-            }
-        }.joinToString("")
-    }.joinToString("\n")
-}
-
-data class Guard(val p: Point2D, val d: Direction) {
-    fun move(map: Map<Point2D, Char>): Guard {
-        val possibleNewPosition = d.move(p)
-        if (map[possibleNewPosition] == '#') {
-            val newDirection = d.turnRight()
-            val newPosition = newDirection.move(p)
-            return Guard(newPosition, newDirection)
-        } else {
-            return Guard(possibleNewPosition, d)
-        }
-    }
-
-    fun isWithinBoundsOf(map: Map<Point2D, Char>): Boolean {
-        return p in map.keys
-    }
-}
-
-data class Point2D(val x: Int, val y: Int)
-enum class Direction {
-    UP {
-        override fun turnLeft() = LEFT
-        override fun turnRight() = RIGHT
-        override fun move(p: Point2D) = Point2D(p.x, p.y - 1)
-    },
-    DOWN {
-        override fun turnLeft() = RIGHT
-        override fun turnRight() = LEFT
-        override fun move(p: Point2D) = Point2D(p.x, p.y + 1)
-    },
-    LEFT {
-        override fun turnLeft() = DOWN
-        override fun turnRight() = UP
-        override fun move(p: Point2D) = Point2D(p.x - 1, p.y)
-    },
-    RIGHT {
-        override fun turnLeft() = UP
-        override fun turnRight() = DOWN
-        override fun move(p: Point2D) = Point2D(p.x + 1, p.y)
-    };
-
-    abstract fun turnLeft(): Direction
-    abstract fun turnRight(): Direction
-    abstract fun move(p: Point2D): Point2D
-}
-
-private fun List<String>.width() = this[0].length
-private fun List<List<String>>.height() = size
 
 class Day06Test {
     @Test
@@ -119,25 +12,23 @@ class Day06Test {
 
     @Test
     fun `part 1 with real input`() {
-        part1(readInputFileToList("day06.txt")) shouldBe -1
+        part1(readInputFileToList("day06.txt")) shouldBe 5242
     }
 
-    @Ignore
     @Test
     fun `part 2 with test input`() {
-        part2(testInput) shouldBe -1
+        part2(testInput) shouldBe 6
     }
 
-    @Ignore
     @Test
     fun `part 2 with real input`() {
-        part2(readInputFileToList("day_template.txt")) shouldBe -1
+        part2(readInputFileToList("day06.txt")) shouldBe 1424
     }
 
     @Test
     fun `generates correct path from test data`() {
         val (map, start) = testInput.parse()
-        val poses = path(start, map)
+        val poses = pathUntilWeFallOffTheMap(start, map)
 
         val posesString = asString(map, poses)
 
@@ -153,8 +44,73 @@ class Day06Test {
             #XXXXXXX..
             ......#X..
         """.trimIndent()
-
     }
+
+    @Test
+    fun `finds correct cycle with tweaked map`() {
+        val (map, start) = inputOf(
+            """
+                ....#.....
+                .........#
+                ..........
+                ..#.......
+                .......#..
+                ..........
+                .#.O^.....
+                ........#.
+                #.........
+                ......#...
+            """
+        )
+
+        asString(map, pathUntilWeDetectACycle(start, map)) shouldBe """
+            ....#.....
+            ....XXXXX#
+            ....X...X.
+            ..#.X...X.
+            ....X..#X.
+            ....X...X.
+            .#.OXXXXX.
+            ........#.
+            #.........
+            ......#...
+        """.trimIndent()
+    }
+
+    @Test
+    fun `finds correct cycle with another tweaked map`() {
+        val (map, start) = inputOf(
+            """
+                ....#.....
+                .........#
+                ..........
+                ..#.......
+                .......#..
+                ..........
+                .#..^.....
+                ......O.#.
+                #.........
+                ......#...
+            """
+        )
+
+        resultsInCycle(start, map) shouldBe true
+
+        asString(map, pathUntilWeDetectACycle(start, map)) shouldBe """
+            ....#.....
+            ....XXXXX#
+            ....X...X.
+            ..#.X...X.
+            ..XXXXX#X.
+            ..X.X.X.X.
+            .#XXXXXXX.
+            ......O.#.
+            #.........
+            ......#...
+        """.trimIndent()
+    }
+
+    private fun inputOf(s: String) = s.trimIndent().lines().parse()
 }
 
 val testInput =

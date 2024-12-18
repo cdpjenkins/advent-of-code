bd12b5ef3e242ce67f0e5d8bcf1ee449588bab62
cdpjenkins <cdpjenkins@gmail.com>
2024-12-18 20:17:06 +0000

    Day 17

---
+++ 2024/kotlin-aoc-2024/src/main/kotlin/day17/Day17.kt
@@ -0,0 +1,155 @@
+package day17
+
+import ListUtils.splitByBlank
+import RegexUtils.parseUsingRegex
+
+fun part1(input: List<String>): String =
+    input.parseToMachineState()
+        .executeUntilHalt()
+        .output
+        .joinToString(",")
+
+fun part2(input: List<String>): Long {
+    val program = input.parseToMachineState().program
+
+    return findValuesOfAThatCauseProgramToPrintItself(program, 0, 1).first()
+}
+
+/**
+ * Note that, for my input at least, there is more than one initial value of register A that causes the program to
+ * print itself. This I did not anticipate.
+ *
+ * Note that I was hoping to come up with an analytical solution (rather than trial and error/brute force, which is what
+ * this method does). But I couldn't figure it out.
+ */
+fun findValuesOfAThatCauseProgramToPrintItself(program: List<Long>, a: Long, length: Int): List<Long> =
+    if (length > program.size) listOf(a)
+    else (0..7)
+        .map { aMod8 -> a.shl(3) or aMod8.toLong() }
+        .filter { attempt -> executeProgramInKotlin(attempt).toList() == program.takeLast(length) }
+        .flatMap { nextA -> findValuesOfAThatCauseProgramToPrintItself(program, nextA, length + 1) }
+
+/**
+ * This is the machine from *my* input, reimplemented in Kotlin for speed and fun.
+ */
+fun executeProgramInKotlin(initialA: Long): Sequence<Long> =
+    sequence {
+        var a = initialA
+
+        while (a > 0) {
+            val aMod8 = a.mod(8L)
+            val shiftMeDoAmount = a shr ((aMod8) xor 3).toInt()
+            val b = ((aMod8 xor 6) xor shiftMeDoAmount).mod(8L)
+            yield(b)
+            a = a shr 3
+        }
+    }
+
+fun List<Long>.disassemble() = this.windowed(2, 2).map { disassembleInstruction(it[0], it[1]) }
+fun disassembleInstruction(instruction: Long, operand: Long) = "${Instruction.of(instruction)}(${operand})"
+
+fun List<String>.parseToMachineState(): ComputerState {
+    val (registersLines, programLines) = splitByBlank()
+
+    val (registerAString) = registersLines[0].parseUsingRegex("""^Register [ABC]: (\d+)$""")
+    val (registerBString) = registersLines[1].parseUsingRegex("""^Register [ABC]: (\d+)$""")
+    val (registerCString) = registersLines[2].parseUsingRegex("""^Register [ABC]: (\d+)$""")
+
+    val registerAValue = registerAString.toLong()
+    val registerBValue = registerBString.toLong()
+    val registerCValue = registerCString.toLong()
+
+    val (programString) = programLines[0].parseUsingRegex("""Program: ([\d,]+)""")
+    val program = programString.split(",").map { it.toLong() }
+
+    return ComputerState(registerAValue, registerBValue, registerCValue, program, programCounter = 0)
+}
+
+enum class Instruction(val opcode: Long) {
+    ADV(0) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                registerA = state.registerA.shr(state.comboOperand().toInt())
+            )
+    },
+    BXL(1) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                registerB = state.registerB xor state.literalOperand()
+            )
+    },
+    BST(2) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                registerB = state.comboOperand().mod(8).toLong(),
+                programCounter = state.programCounter + 2
+            )
+    },
+    JNZ(3) {
+        override fun execute(state: ComputerState) =
+            if (state.registerA == 0L) state.copy(programCounter = state.programCounter + 2)
+            else state.copy(programCounter = state.literalOperand().toInt())
+    },
+    BXC(4) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                registerB = state.registerB xor state.registerC
+            )
+    },
+    OUT(5) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                output = state.output + state.comboOperand().mod(8)
+            )
+    },
+    BDV(6) {
+        override fun execute(state: ComputerState) =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                registerB = state.registerA / 1.shl(state.comboOperand().toInt())
+            )
+    },
+    CDV(7) {
+        override fun execute(state: ComputerState): ComputerState =
+            state.copy(
+                programCounter = state.programCounter + 2,
+                registerC = state.registerA / 1.shl(state.comboOperand().toInt())
+            )
+    };
+
+    abstract fun execute(state: ComputerState): ComputerState
+
+    companion object {
+        fun of(opcode: Long) = entries.first { it.opcode == opcode }
+    }
+}
+
+data class ComputerState(
+    val registerA: Long,
+    val registerB: Long,
+    val registerC: Long,
+    val program: List<Long>,
+    val programCounter: Int = 0,
+    val output: List<Int> = emptyList()
+) {
+    fun comboOperand() =
+        program[programCounter + 1].let {
+            when (it) {
+                in (0..3) -> it
+                4L -> registerA
+                5L -> registerB
+                6L -> registerC
+                else -> throw IllegalStateException("Invalid combo: $it")
+            }
+        }
+    fun executeUntilHalt(): ComputerState =
+        generateSequence(this) { it.executeOneInstruction() }
+            .first { it.isHalted() }
+    fun executeOneInstruction() = Instruction.of(program[programCounter]).execute(this)
+    fun isHalted() = programCounter >= program.size
+    fun literalOperand() = program[programCounter+1]
+}
---
+++ 2024/kotlin-aoc-2024/src/test/kotlin/day17/Day17Test.kt
@@ -0,0 +1,137 @@
+package day17
+
+import FileUtil.readInputFileToList
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class Day17Test {
+    @Test
+    fun `part 1 with test input`() {
+        part1(testInput) shouldBe "4,6,3,5,6,3,5,2,1,0"
+    }
+
+    @Test
+    fun `part 1 with real input`() {
+        part1(readInputFileToList("day17.txt")) shouldBe "5,1,3,4,3,7,2,1,7"
+    }
+
+    @Test
+    fun `part 2 with real input`() {
+        part2(readInputFileToList("day17.txt")) shouldBe 216584205979245L
+    }
+
+    @Test
+    fun `executing bst instruction writes operand modulo 8 to register B`() {
+        val state = """
+            Register A: 0
+            Register B: 0
+            Register C: 9
+
+            Program: 2,6
+        """.trimIndent().lines().parseToMachineState()
+
+        val newState = state.executeUntilHalt()
+
+        newState shouldBe state.copy(
+            registerA = 0,
+            registerB = 1,
+            registerC = 9,
+            programCounter = 2
+        )
+    }
+
+    @Test
+    fun `executing out instruction outputs combo operand modulo 8`() {
+        val state = """
+            Register A: 10
+            Register B: 0
+            Register C: 0
+
+            Program: 5,0,5,1,5,4
+        """.trimIndent().lines().parseToMachineState()
+
+        val newState = state.executeUntilHalt()
+
+        newState shouldBe state.copy(
+            registerA = 10,
+            registerB = 0,
+            registerC = 0,
+            programCounter = 6,
+            output = listOf(0, 1, 2)
+        )
+    }
+
+    @Test
+    fun `jnz jumps if A is non-zero`() {
+        val state = """
+            Register A: 2024
+            Register B: 0
+            Register C: 0
+
+            Program: 0,1,5,4,3,0
+        """.trimIndent().lines().parseToMachineState()
+
+        val newState = state.executeUntilHalt()
+
+        newState.output shouldBe listOf(4,2,5,6,7,7,7,7,3,1,0)
+        newState.registerA shouldBe 0
+    }
+
+    @Test
+    fun `bxl computes xor or b and literal arg and puts result in b`() {
+        val state = """
+            Register A: 0
+            Register B: 29
+            Register C: 0
+
+            Program: 1,7
+        """.trimIndent().lines().parseToMachineState()
+
+        val newState = state.executeUntilHalt()
+
+        newState.registerB shouldBe 26
+    }
+
+    @Test
+    fun `bxc computers xor of b and c and puts result in b`() {
+        val state = """
+            Register A: 0
+            Register B: 2024
+            Register C: 43690
+
+            Program: 4,0
+        """.trimIndent().lines().parseToMachineState()
+
+        val newState = state.executeUntilHalt()
+
+        newState.registerB shouldBe 44354
+    }
+
+    @Test
+    fun `can execute the program using the simulated computer`() {
+        val state = """
+            Register A: 216584205979245
+            Register B: 0
+            Register C: 0
+            
+            Program: 2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0
+        """.trimIndent().lines().parseToMachineState()
+
+        state.executeUntilHalt().output shouldBe listOf(2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0)
+    }
+
+    @Test
+    fun `can execute the program in kotlin`() {
+        val output = executeProgramInKotlin(initialA = 216584205979327).toList()
+        output shouldBe listOf(2,4,1,3,7,5,1,5,0,3,4,2,5,5,3,0)
+    }
+}
+
+val testInput =
+    """
+        Register A: 729
+        Register B: 0
+        Register C: 0
+
+        Program: 0,1,5,4,3,0
+    """.trimIndent().lines()

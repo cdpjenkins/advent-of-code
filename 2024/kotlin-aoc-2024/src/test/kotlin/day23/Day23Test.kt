bd605cd0c61aa0016b7056a227d7ec7f007d658a
Chris Jenkins <cdpjenkins@gmail.com>
2024-12-25 00:23:06 +0000

    Day 23

--- 2024/kotlin-aoc-2024/src/test/kotlin/day23/Day23Test.kt
+++ 2024/kotlin-aoc-2024/src/test/kotlin/day23/Day23Test.kt
@@ -4,93 +4,115 @@ import utils.FileUtil.readInputFileToList
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import utils.RegexUtils.parseUsingRegex
-import kotlin.test.Ignore
 
-private fun part1(input: List<String>): Int {
-    return 123
+private fun part1(input: List<String>) =
+    input.parse()
+        .findTriples()
+        .count { atLeastOneNodeStartsWithLetterT(it) }
+
+private fun part2(input: List<String>) =
+    input.parse()
+        .findCliques()
+        .maxBy { it.size }
+        .toList()
+        .sorted()
+        .joinToString(",")
+
+private fun atLeastOneNodeStartsWithLetterT(it: Triple<String, String, String>) =
+    it.first.startsWith("t") ||
+            it.second.startsWith("t") ||
+            it.third.startsWith("t")
+
+private fun MutableMap<String, Set<String>>.findTriples(): List<Triple<String, String, String>> {
+    val triples = keys.flatMap { n1 ->
+        val n1Neighbours = getValue(n1) //.filter { it > n1 }
+        n1Neighbours.flatMap { n2 ->
+            val n2Neighbours = getValue(n2)
+            n2Neighbours
+                .filter { it in n1Neighbours }
+                .map { n3 -> Triple(n1, n2, n3) }
+                .filter { it.isInOrder() }
+        }
+    }
+    return triples
 }
 
-private fun part2(input: List<String>): Int {
-    return 123
-}
+// I'm not actually sure if "clique" is the right name in graph theory for a subgraph where there exists an edge between
+// every pair of nodes in the subgraph. Whatever, that's the name that I'm using here.
+private fun Map<String, Set<String>>.findCliques(): MutableSet<Set<String>> {
+    var visited = mutableSetOf<String>()
+    val subGraphs = mutableSetOf<Set<String>>()
 
-class Day23Test {
-    @Ignore
-    @Test
-    fun `part 1 with test input`() {
-        part1(testInput) shouldBe -1
-    }
-
-    @Ignore
-    @Test
-    fun `part 1 with real input`() {
-        part1(readInputFileToList("day_template.txt")) shouldBe -1
-    }
-
-    @Ignore
-    @Test
-    fun `part 2 with test input`() {
-        part2(testInput) shouldBe -1
-    }
-
-    @Ignore
-    @Test
-    fun `part 2 with real input`() {
-        part2(readInputFileToList("day_template.txt")) shouldBe -1
-    }
-
-    @Test
-    fun `finds strongly connected components`() {
-        val REGEX = """([a-z]{2})-([a-z]{2})""".toRegex()
-        val graph = testInput.flatMap {
-            val (node1, node2) = it.parseUsingRegex(REGEX)
-            listOf(
-                node1 to node2,
-                node2 to node1
-            )
-        }.toMap()
-        // ^^^^ gaaa no we need a multimap
-        // or Map<String, Set<String>> !
-
-        graph.findStronglyConnectComponents()
-
-    }
-}
-
-private fun Map<String, String>.findStronglyConnectComponents() {
-    // Tarjan's strongly connected components algorithm
-    // see https://en.wikipedia.org/wiki/Tarjan%27s_strongly_connected_components_algorithm
-
-
-
-    val s = ArrayDeque<String>()
-    val indices = mutableMapOf<String, Int>()
-    val lowlinks = mutableMapOf<String, Int>()
-    val onStack = mutableSetOf<String>()
-    var index = 0
-
-
-    fun strongConnect(v: String) {
-        indices[v] = index
-        lowlinks[v] = index
-        index++
-
-        s.addLast(v)
-        onStack.add(v)
-
-        // Consider successors of v
-        // TODO you are here
-        // but remember to represent the graph as a multimap first
-    }
-
-    this.keys.forEach { v ->
-        if (!indices.containsKey(v)) {
-            strongConnect(v)
+    fun visitMeDo(newNode: String, currentSubgraph: MutableSet<String>) {
+        if (newNode !in visited) {
+            val neighbours = this.getValue(newNode)
+            if (currentSubgraph.all { it in neighbours }) {
+                // yay, same clique subgraph thang
+                currentSubgraph.add(newNode)
+                visited.add(newNode)
+                neighbours.forEach { neighbour ->
+                    visitMeDo(neighbour, currentSubgraph)
+                }
+            }
+            // else nope, it's not in the same subGraph
         }
     }
 
+    this.keys.forEach { currentNode ->
+        visited = mutableSetOf()
+        val currentSubgraph = mutableSetOf(currentNode)
+        visited.add(currentNode)
 
+        this.getValue(currentNode).forEach { neighbour ->
+            if (neighbour !in visited) {
+                visitMeDo(neighbour, currentSubgraph)
+            }
+        }
+        subGraphs.add(currentSubgraph)
+    }
+
+    return subGraphs
 }
+
+
+private fun List<String>.parse(): MutableMap<String, Set<String>> {
+    val REGEX = """([a-z]{2})-([a-z]{2})""".toRegex()
+    val tharMap = mutableMapOf<String, Set<String>>().withDefault { setOf() }
+    forEach {
+        val (node1, node2) = it.parseUsingRegex(REGEX)
+        tharMap[node1] = tharMap.getValue(node1) + node2
+        tharMap[node2] = tharMap.getValue(node2) + node1
+    }
+    return tharMap
+}
+
+
+class Day23Test {
+    @Test
+    fun `part 1 with test input`() {
+        part1(testInput) shouldBe 7
+    }
+
+    @Test
+    fun `part 1 with real input`() {
+        part1(readInputFileToList("day23.txt")) shouldBe 1378
+    }
+
+    @Test
+    fun `part 2 with test input`() {
+        part2(testInput) shouldBe "co,de,ka,ta"
+    }
+
+    @Test
+    fun `part 2 with real input`() {
+        part2(readInputFileToList("day23.txt")) shouldBe "bs,ey,fq,fy,he,ii,lh,ol,tc,uu,wl,xq,xv"
+    }
+}
+
+private fun Triple<String, String, String>.isInOrder(): Boolean {
+    return this.first < this.second  && this.second < this.third
+}
+
 
 val testInput =
     """

ee57fbfaf41fd25ca1c54d27e0fe95b19aec9792
Chris Jenkins <cdpjenkins@gmail.com>
2025-12-04 05:45:40 +0000

    Day 4 part 2 messy

--- 2025/kotlin-aoc-2025/src/test/kotlin/day04/Day04Test.kt
+++ 2025/kotlin-aoc-2025/src/test/kotlin/day04/Day04Test.kt
@@ -7,15 +7,40 @@ import utils.Vector2D
 import kotlin.test.Ignore
 
 private fun part1(input: List<String>): Int {
-    val bales = input.withIndex().flatMap { (y, row) ->
-        row.withIndex().map { (x, c) ->
-            if (c == '@') Vector2D(x, y) else null
-        }
-    }.filterNotNull()
-        .toSet()
+    val bales = parseToBales(input)
 
-    return bales.count { it.numNeighboursIn(bales) < 4 }
+    return bales.count { it.canRemove(bales) }
 }
+
+private fun Vector2D.canRemove(bales: Set<Vector2D>): Boolean = numNeighboursIn(bales) < 4
+
+private fun part2(input: List<String>): Int {
+    val initialBales = parseToBales(input)
+    var bales = initialBales
+    var done = false
+
+    while (!done) {
+        val newBales = bales.filter { !it.canRemove(bales) }.toSet()
+        if (newBales.size < bales.size) {
+            bales = newBales
+        } else {
+            done = true
+        }
+    }
+
+    println(bales.size)
+    println(initialBales.size)
+
+    return initialBales.size - bales.size
+}
+
+
+private fun parseToBales(input: List<String>): Set<Vector2D> = input.withIndex().flatMap { (y, row) ->
+    row.withIndex().map { (x, c) ->
+        if (c == '@') Vector2D(x, y) else null
+    }
+}.filterNotNull()
+    .toSet()
 
 private fun Vector2D.numNeighboursIn(bales: Set<Vector2D>): Int {
     return this.neighbours().count { it in bales }
@@ -35,9 +60,6 @@ private fun Vector2D.neighbours(): Set<Vector2D> {
 }
 
 
-private fun part2(input: List<String>): Int {
-    return 123
-}
 
 class Day04Test {
 
@@ -48,16 +70,14 @@ class Day04Test {
 
     @Test
     fun `part 1 with real input`() {
-        part1(readInputFileToList("day04.txt")) shouldBe -1
+        part1(readInputFileToList("day04.txt")) shouldBe 1397
     }
 
-    @Ignore
     @Test
     fun `part 2 with test input`() {
-        part2(testInput) shouldBe -1
+        part2(testInput) shouldBe 43
     }
 
-    @Ignore
     @Test
     fun `part 2 with real input`() {
         part2(readInputFileToList("day04.txt")) shouldBe -1

b9ef8fc92c8e549a7df6a3e0c862d9620f53542b
Chris Jenkins <cdpjenkins@gmail.com>
2025-12-05 09:42:42 +0000

    Day 5 part 2

--- 2025/kotlin-aoc-2025/src/test/kotlin/day05/Day05Test.kt
+++ 2025/kotlin-aoc-2025/src/test/kotlin/day05/Day05Test.kt
@@ -4,30 +4,58 @@ import utils.FileUtil.readInputFileToList
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import utils.ListUtils.splitByBlank
-import kotlin.test.Ignore
+import kotlin.math.max
+import kotlin.math.min
 
 private fun part1(input: List<String>): Int {
+    val (freshIngredientRanges, availableIngredientIds) = input.parse()
 
-    val (freshIngredientIdRangesLines, availableIngredientIdsLines) = input.splitByBlank()
+    return availableIngredientIds.count { id ->
+        freshIngredientRanges.any { id in it }
+    }
+}
+
+private fun part2(input: List<String>): Long {
+    val (freshIngredientRanges, _) = input.parse()
+
+    return mergedRanges(freshIngredientRanges).sumOf { it.last - it.first + 1 }
+}
+
+private fun mergedRanges(freshIngredientRanges: List<LongRange>): MutableList<LongRange> {
+    val rangesToMerge = freshIngredientRanges.sortedBy { it.first }
+    val mergedRanges = mutableListOf<LongRange>()
+
+    rangesToMerge.forEach { range ->
+        if (mergedRanges.isEmpty()) {
+            mergedRanges.add(range)
+        } else {
+            val lastRange = mergedRanges.last()
+
+            if (range.canMergeInto(lastRange)) {
+                val mergedLastRange = lastRange.mergeWith(range)
+                mergedRanges.removeLast()
+                mergedRanges.add(mergedLastRange)
+            } else {
+                mergedRanges.add(range)
+            }
+        }
+    }
+    return mergedRanges
+}
+
+private fun LongRange.canMergeInto(lastRange: LongRange?): Boolean = lastRange == null || first in lastRange
+private fun LongRange.mergeWith(that: LongRange): LongRange {
+    return min(this.first, that.first)..max(this.last, that.last)
+}
+
+private fun List<String>.parse(): Pair<List<LongRange>, List<Long>> {
+    val (freshIngredientIdRangesLines, availableIngredientIdsLines) = splitByBlank()
     val freshIngredientRanges = freshIngredientIdRangesLines.map {
         val (startString, endString) = it.split("-")
         startString.toLong()..endString.toLong()
     }
     val availableIngredientIds = availableIngredientIdsLines.map { it.toLong() }
-
-
-    val numFreshIngredients = availableIngredientIds.count { id ->
-        freshIngredientRanges.any { id in it }
-    }
-
-
-
-
-    return numFreshIngredients
-}
-
-private fun part2(input: List<String>): Int {
-    return 123
+    return Pair(freshIngredientRanges, availableIngredientIds)
 }
 
 class Day05Test {
@@ -42,16 +70,14 @@ class Day05Test {
         part1(readInputFileToList("day05.txt")) shouldBe 643
     }
 
-    @Ignore
     @Test
     fun `part 2 with test input`() {
-        part2(testInput) shouldBe -1
+        part2(testInput) shouldBe 14
     }
 
-    @Ignore
     @Test
     fun `part 2 with real input`() {
-        part2(readInputFileToList("day_template.txt")) shouldBe -1
+        part2(readInputFileToList("day05.txt")) shouldBe 342018167474526
     }
 }
 

3540cad2843233a91970f01bf58006f14ce6de55
Chris Jenkins <cdpjenkins@gmail.com>
2025-12-05 08:49:54 +0000

    Day 4 different and slight faster approach

--- 2025/kotlin-aoc-2025/src/test/kotlin/day04/Day04Test.kt
+++ 2025/kotlin-aoc-2025/src/test/kotlin/day04/Day04Test.kt
@@ -4,7 +4,6 @@ import utils.FileUtil.readInputFileToList
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
 import utils.Vector2D
-import kotlin.test.Ignore
 
 private fun part1(input: List<String>): Int {
     val bales = parseToBales(input)
@@ -15,36 +14,29 @@ private fun part1(input: List<String>): Int {
 private fun Vector2D.canRemove(bales: Set<Vector2D>): Boolean = numNeighboursIn(bales) < 4
 
 private fun part2(input: List<String>): Int {
-    val initialBales = parseToBales(input)
-    var bales = initialBales
-    var done = false
+    val (initialBales, numNeighboursOf) = parseToBalesWithNeighboursPrecomputed(input)
 
-    while (!done) {
-        val newBales = bales.filter { !it.canRemove(bales) }.toSet()
-        if (newBales.size < bales.size) {
-            bales = newBales
-        } else {
-            done = true
+    val bales = initialBales.toMutableSet()
+    val candidates = initialBales.toMutableList()
+
+    while (!candidates.isEmpty()) {
+        val candidate = candidates.removeLast()
+
+        if (candidate in bales && candidate.canRemoveAccordingTo(numNeighboursOf)) {
+            bales.remove(candidate)
+
+            val neighbours = candidate.neighbours()
+            neighbours.forEach { numNeighboursOf[it] = numNeighboursOf.getValue(it) - 1 }
+            candidates.addAll(neighbours)
         }
     }
-
-    println(bales.size)
-    println(initialBales.size)
 
     return initialBales.size - bales.size
 }
 
+private fun Vector2D.canRemoveAccordingTo(numNeighboursMap: Map<Vector2D, Int>) = numNeighboursMap.getValue(this) < 4
 
-private fun parseToBales(input: List<String>): Set<Vector2D> = input.withIndex().flatMap { (y, row) ->
-    row.withIndex().map { (x, c) ->
-        if (c == '@') Vector2D(x, y) else null
-    }
-}.filterNotNull()
-    .toSet()
-
-private fun Vector2D.numNeighboursIn(bales: Set<Vector2D>): Int {
-    return this.neighbours().count { it in bales }
-}
+private fun Vector2D.numNeighboursIn(bales: Set<Vector2D>) = this.neighbours().count { it in bales }
 
 private fun Vector2D.neighbours(): Set<Vector2D> {
     return setOf(
@@ -59,7 +51,48 @@ private fun Vector2D.neighbours(): Set<Vector2D> {
     )
 }
 
+private fun parseToBales(input: List<String>): Set<Vector2D> {
+    return input.withIndex().flatMap { (y, row) ->
+        row.withIndex().map { (x, c) ->
+            if (c == '@') Vector2D(x, y) else null
+        }
+    }.filterNotNull()
+        .toSet()
+}
 
+private fun parseToBalesWithNeighboursPrecomputed(input: List<String>): Pair<Set<Vector2D>, MutableMap<Vector2D, Int>> {
+    val width = input.first().length
+    val height = input.size
+
+    val numNeighbours = mapOf<Vector2D, Int>().toMutableMap().withDefault { 0 }
+
+    val numNeighboursArray = List(width * height) { 0 }.toMutableList()
+
+    val bales = input.withIndex().flatMap { (y, row) ->
+        row.withIndex().map { (x, c) ->
+            if (c == '@') {
+                val pos = Vector2D(x, y)
+
+                pos.neighbours().filter { it.isInBounds(width, height) }.forEach {
+                    numNeighbours[it] = numNeighbours.getValue(it) + 1
+
+                    numNeighboursArray[width * y + x]++
+                }
+
+                pos
+            } else {
+                null
+            }
+        }
+    }.filterNotNull()
+        .toSet()
+    return Pair(bales, numNeighbours)
+}
+
+private fun Vector2D.isInBounds(width: Int, height: Int): Boolean {
+    return x >= 0 && x < width &&
+            y >= 0 && y < height
+}
 
 class Day04Test {
 
@@ -80,7 +113,7 @@ class Day04Test {
 
     @Test
     fun `part 2 with real input`() {
-        part2(readInputFileToList("day04.txt")) shouldBe -1
+        part2(readInputFileToList("day04.txt")) shouldBe 8758
     }
 }
 
